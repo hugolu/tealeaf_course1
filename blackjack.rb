dc38ee14aa51044bbd8ff82c310a9c3640e901db
Hugo Lu <printk@gmail.com>
2015-03-24 22:10:11 +0800

    patch the logic

--- blackjack.rb
+++ blackjack.rb
@@ -9,8 +9,9 @@
 def have_new_cards
   cards = []
   suits = ["♣", "♦", "♥", "♠"]
+  ranks = ["A","2","3","4","5","6","7","8","9","10","J","Q","K"]
   suits.each do |suit|
-    (1..13).each do |rank|
+    ranks.each do |rank|
       cards << [suit, rank]
     end
   end
@@ -22,28 +23,29 @@ def deal_a_card(cards)
 end
 
 def show_cards(cards)
-  cards.each do |(suit,rank)|
-    print "#{suit}"
-    case rank
-    when 1
-      print "A"
-    when 2..10
-      print "#{rank}"
-    when 11
-      print "J"
-    when 12
-      print "Q"
-    when 13
-      print "K"
-    end
-    print " "
+  cards.each do |(suit, rank)|
+    print "#{suit}#{rank}, "
   end
 end
 
 def calc_sum(cards)
   sum = 0
-  cards.each do |(suit, rank)|
-    sum += rank
+  aces = 0
+  cards.each do |(_, rank)|
+    case rank
+    when 'A'
+      sum += 11
+      aces += 1
+    when 'J','Q','K'
+      sum += 10
+    else
+      sum += rank.to_i
+    end
+  end
+
+  while sum > 21 && aces > 0
+    sum -= 10
+    aces -= 1
   end
   puts "sum: #{sum}"
   return sum
@@ -62,42 +64,57 @@ def player_choice(cards)
   end
 end
 
-# 1. init the poker cards
-cards = have_new_cards
-player_cards = []
-dealer_cards = []
-
-player_sum = 0
-dealer_sum = 0
-
-# 2. deal a card to player and dealer
-player_cards << deal_a_card(cards)
-show_cards(player_cards)
-player_sum = calc_sum(player_cards)
-
-dealer_cards << deal_a_card(cards)
-show_cards(dealer_cards)
-dealer_sum = calc_sum(dealer_cards)
-
-# 3. the player goes first and can choose to either "hit" or "stay" 
-until player_sum > 21
-  card = player_choice(cards)
-  if card == nil
-    break
+def play_again?
+  loop do
+    puts "play again?"
+    ans = gets.chomp.downcase
+    return true if ans == 'y'
+    return false if ans == 'n'
   end
-  player_cards << card
-  show_cards(player_cards)
-  p player_sum = calc_sum(player_cards)
 end
 
-# 4. the dealer must hit until she has at least 17
-until dealer_sum > 21
-  if dealer_sum >= 17 and dealer_sum > player_sum
-    break
-  end
+loop do
+  # 0. welcome
+  puts "Welcome to Black Jack"
+
+  # 1. init the poker cards
+  cards = have_new_cards
+  player_cards = []
+  dealer_cards = []
+
+  player_sum = 0
+  dealer_sum = 0
+
+  # 2. deal a card to player and dealer
+  player_cards << deal_a_card(cards)
+  show_cards(player_cards)
+  player_sum = calc_sum(player_cards)
+
   dealer_cards << deal_a_card(cards)
   show_cards(dealer_cards)
+  dealer_sum = calc_sum(dealer_cards)
 
-  p dealer_sum = calc_sum(dealer_cards)
+  # 3. the player goes first and can choose to either "hit" or "stay" 
+  until player_sum >= 21
+    card = player_choice(cards)
+    if card == nil
+      break
+    end
+    player_cards << card
+    show_cards(player_cards)
+    player_sum = calc_sum(player_cards)
+  end
+
+  # 4. the dealer must hit until she has at least 17
+  until dealer_sum >= 21
+    if dealer_sum >= 17 and dealer_sum > player_sum
+      break
+    end
+    dealer_cards << deal_a_card(cards)
+    show_cards(dealer_cards)
+    dealer_sum = calc_sum(dealer_cards)
+  end
+
+  # 5. play again?
+  exit if play_again? == false
 end
-
